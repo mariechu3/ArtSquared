d379c8fdadcb58fd2f37ce64ea03fc6b76967631
Leyth Toubassy <lizardman1028@gmail.com>
2023-11-21 14:28:37 -0800

    Added basic canvas functionality

--- app/screens/Canvas.js
+++ app/screens/Canvas.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TouchableWithoutFeedback } from 'react-native'
 import { StyleSheet, View, Text } from 'react-native'
+
 
 export default Canvas = ({ navigation, route }) => (
   <View style={styles.screen}>
     <Text>Canvas</Text>
     {/* <LinkFarm navigation={navigation} route={route} /> */}
+    <DrawingCanvas/>
   </View>
 )
+
+function Pixel() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  _onPressButton = () => {
+    setIsClicked(!isClicked);
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={this._onPressButton} >
+      <View style={styles.pixel} backgroundColor={isClicked ? "#000000" : "#ffffff"}>
+       {/* <Text style={styles.buttonText}>TouchableWithoutFeedback</Text> */}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+Row = props => {
+  return (
+    <View style={styles.row}>
+      <Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/><Pixel/>
+    </View>
+  );
+}
+
+DrawingCanvas = props => {
+  return (
+    <View style={styles.drawingCanvas}>
+      <Row/>
+      <Row/>
+      <Row/>
+      <Row/>
+      <Row/>
+      <Row/>
+      <Row/>
+      <Row/>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   screen: {
@@ -21,5 +63,24 @@ const styles = StyleSheet.create({
     width: 44,
     height: 44,
 
-  }
+  },
+  pixel: {
+    width: 44,
+    height: 44,
+    alignItems: 'center',
+    backgroundColor: '#2196F3',
+    flexDirection:"row",
+  },
+  row: {
+    alignItems: 'center',
+    flexDirection:"row",
+  },
+  drawingCanvas: {
+    alignItems: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'white',
+  },
 })
