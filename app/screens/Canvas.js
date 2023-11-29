d850e1cb5937b81abbaef9f602293fe8f59711cc
Leyth Toubassy <lizardman1028@gmail.com>
2023-11-29 13:43:16 -0800

    added draw and erase tool funtionality to canvas

--- app/screens/Canvas.js
+++ app/screens/Canvas.js
@@ -1,7 +1,17 @@
 import React, { useState } from 'react'
-import { TouchableWithoutFeedback } from 'react-native'
+
+
+import { Button, TouchableWithoutFeedback } from 'react-native'
 import { StyleSheet, View, Text } from 'react-native'
 
+selectedColor = "#000000"
+eraseColor = "#ffffff"
+selectedTool = 0
+/*~~~~~~~~~~~*\
+| tool codes  |
+| 0 -> draw   |
+| 1 -> erase  |
+\*~~~~~~~~~~~*/
 
 export default Canvas = ({ navigation, route }) => (
   <View style={styles.screen}>
@@ -11,21 +21,45 @@ export default Canvas = ({ navigation, route }) => (
   </View>
 )
 
+
+_onColorButtonPress = () => {
+  selectedColor = "#ff0000"
+}
+
+
+_onDrawButtonPress = () => {
+  selectedTool = 0
+}
+
+
+_onEraseButtonPress = () => {
+  selectedTool = 1
+}
+
+
 function Pixel() {
-  const [isClicked, setIsClicked] = useState(false);
+  const [curColor, setCurColor] = useState("#ffffff");
+
 
   _onPressButton = () => {
-    setIsClicked(!isClicked);
+    if (selectedTool == 0) {
+      setCurColor(selectedColor);
+    }
+    if (selectedTool == 1) {
+      setCurColor(eraseColor);
+    }
   }
+
 
   return (
     <TouchableWithoutFeedback onPress={this._onPressButton} >
-      <View style={styles.pixel} backgroundColor={isClicked ? "#000000" : "#ffffff"}>
+      <View style={styles.pixel} backgroundColor={curColor}>
        {/* <Text style={styles.buttonText}>TouchableWithoutFeedback</Text> */}
       </View>
     </TouchableWithoutFeedback>
   );
 };
+
 
 Row = props => {
   return (
@@ -34,6 +68,55 @@ Row = props => {
     </View>
   );
 }
+
+
+function Tools() {
+  const [drawCurColor, setDrawCurColor] = useState(selectedColor);
+  const [eraseCurColor, setEraseCurColor] = useState("#aaaaaa");
+ 
+  nonSelectColor = "#aaaaaa"
+  selectEraseColor = "#555555"
+
+
+  _onDrawButtonPressLocal = () => {
+    this._onDrawButtonPress()
+    this._onToolButtonPress();
+  }
+ 
+  _onEraseButtonPressLocal = () => {
+    this._onEraseButtonPress()
+    this._onToolButtonPress();
+  }
+
+
+  _onToolButtonPress = () => {
+    if (selectedTool == 0) {
+      setDrawCurColor(selectedColor);
+      setEraseCurColor(nonSelectColor)
+    }
+    if (selectedTool == 1) {
+      setDrawCurColor(nonSelectColor);
+      setEraseCurColor(selectEraseColor)
+    }
+  }
+
+
+  return (
+    <View style={styles.toolbar}>
+      <TouchableWithoutFeedback onPress={this._onDrawButtonPressLocal} >
+        <View style={styles.tool} backgroundColor={drawCurColor}>
+          <Text style={styles.buttonText}>Draw</Text>
+        </View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPress={this._onEraseButtonPressLocal} >
+        <View style={styles.tool} backgroundColor={eraseCurColor}>
+          <Text style={styles.buttonText}>Erase</Text>
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
+
 
 DrawingCanvas = props => {
   return (
@@ -46,9 +129,13 @@ DrawingCanvas = props => {
       <Row/>
       <Row/>
       <Row/>
+      <View style={styles.toolbar}>
+        <Tools/>
+      </View>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   screen: {
@@ -63,13 +150,24 @@ const styles = StyleSheet.create({
     width: 44,
     height: 44,
 
+
   },
   pixel: {
     width: 44,
     height: 44,
+    margin: 1,
+    marginHorizontal: 1,
     alignItems: 'center',
     backgroundColor: '#2196F3',
     flexDirection:"row",
+  },
+  tool: {
+    height: 44,
+    alignItems: 'center',
+    backgroundColor: '#2196F3',
+    flexDirection:"row",
+    marginHorizontal: 5,
+    paddingHorizontal: 10,
   },
   row: {
     alignItems: 'center',
@@ -78,9 +176,15 @@ const styles = StyleSheet.create({
   drawingCanvas: {
     alignItems: 'center',
   },
+  toolbar: {
+    flexDirection:"row",
+    margin: 5,
+  },
   buttonText: {
     textAlign: 'center',
-    padding: 20,
+    padding: 10,
     color: 'white',
   },
 })
+
+
