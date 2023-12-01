57da441d79911b454293ea221369bb3d7d558018
Marie Chu <mariechu3@gmail.com>
2023-11-30 22:40:40 -0800

    fix

--- app/components/Friend.js
+++ app/components/Friend.js
@@ -19,7 +19,7 @@ export default Friend = ({ name, picture, addFriend, removeFriend }) => {
         <TouchableOpacity style={{ display: 'flex', alignItems: 'center', width: 100, position: 'relative' }} onPress={() => setSelected(!selected)}>
             <Image style={{ width: 100, height: 100, borderRadius: 50 }} source={picture} />
             <View style={{ display: 'flex', flexDirection: "row", alignItems: "center" }}>
-                <Text style={{fontSize: 18}}>{name}</Text>
+                <Text style={{ fontSize: 18 }}>{name}</Text>
                 {selected && <Icon size={20} type="ionicon" name="checkmark-circle-outline" />}
             </View>
         </TouchableOpacity >
